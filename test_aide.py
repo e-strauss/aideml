6b781b7e6b7d67296ddbeb84b60690ce1a79d53a
e-strauss <elias.strauss@tu-berlin.de>
2025-10-27 11:26:17 +0100

    updated run script

--- test_aide.py
+++ test_aide.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
 
     load_dotenv(override=True)
     # exp = Experiment(data_dir="input", goal="Predict the sales price for each house", eval="Use the RMSE metric between the logarithm of the predicted and observed values.")
-    exp = Experiment(data_dir="input", goal="Predict the surface",
-                     eval="use a metric which is suitable for classification")
+    exp = Experiment(data_dir="input", goal="find the goal column from the sample submission")
 
-    exp.run(10)
+    exp.run(35)
